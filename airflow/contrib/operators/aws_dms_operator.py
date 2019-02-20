a25141d1c549de950fbf239b4ef6c9cfd937b93a
Khang Pham <vkhang55@gmail.com>
2019-02-20 01:46:09 -0800

    added aws dms support

---
+++ airflow/contrib/operators/aws_dms_operator.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+import sys
+
+from math import pow
+from time import sleep
+
+from airflow.exceptions import AirflowException
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+from airflow.contrib.hooks.aws_hook import AwsHook
+
+
+class AWSDMSOperator(BaseOperator):
+    """
+    Execute a task on AWS Database Migration System (DMS)
+    """
+
+    ui_color = '#c3dae0'
+    client = None
+    arn = None
+    template_fields = ('job_name', 'overrides',)
+
+    @apply_defaults
+    def __init__(self, job_name, job_definition, job_queue, overrides, max_retries=4200,
+                 aws_conn_id=None, region_name=None, **kwargs):
+        super(AWSDMSOperator, self).__init__(**kwargs)
+
+    def execute(self, context):
+      # Code
+
+    def _wait_for_task_ended(self):
+      # Code
+
+    def _check_success_task(self):
+      # Code
+
+    def get_hook(self):
+      # Code
+
+    def on_kill(self):
+      # Code
